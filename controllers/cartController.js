074ecee57938441ffba540429011acb74f9dc7c5
pavithraram <pavithra.r2022ece@sece.ac.in>
2024-08-13 21:54:06 +0530

    feat:cors

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -64,40 +64,81 @@
 const express=require("express")
 const cartmodel=require("../models/cartModel")
 
-exports.postcart=async (req,res)=>{
-    const {userid} =req.user.userid;
+// exports.postcart=async (req,res)=>{
+//     const {userid} =req.user.userid;
+//     const { product_id, quantity } = req.body;
+    
+//     const user=await cartmodel.findOne({userid})
+//     if(!user){
+//         const newuser=new cartmodel({
+//             userid,
+//             products:[{
+//                 productid:req.body.productid,
+//                 quantity:req.body.quantity
+//             }],
+//         })
+
+//         await newuser.save()
+//         res.json("cart created")
+//     }else{
+//         const existingproduct=user.products.find((product)=>product.productid===req.body.productid)
+//         if(existingproduct){
+//             existingproduct.quantity+=req.body.quantity;
+//         }else{
+//             const newproducts=user.products
+//             newproducts.push({
+//                 productid:req.body.productid,
+//                 quantity:req.body.quantity
+//             })
+//         }
+//         user.save()
+//         res.json("cart updated")
+//     }
+// }
+exports.postcart = async (req, res) => {
+    const { userid } = req.user; // Assuming req.user contains user information
     const { product_id, quantity } = req.body;
-    const user=await cartmodel.findOne({userid})
-   
-    
-    if(!user){
-        const newuser=new cartmodel({
-            userid,
-            products:[{
-                productid:req.body.productid,
-                quantity:req.body.quantity
-            }],
-        })
-
-        await newuser.save()
-        res.json("cart created")
-    }else{
-        const existingproduct=user.products.find((product)=>product.productid===req.body.productid)
-        if(existingproduct){
-            existingproduct.quantity+=req.body.quantity;
-        }else{
-            const newproducts=user.products
-            newproducts.push({
-                productid:req.body.productid,
-                quantity:req.body.quantity
-            })
+  
+    try {
+      let userCart = await cartModel.findOne({ userid });
+  
+      if (!userCart) {
+        // Create a new cart if it doesn't exist
+        const newCart = new cartModel({
+          userid,
+          products: [
+            {
+              product_id,
+              quantity,
+            },
+          ],
+        });
+        await newCart.save();
+        return res.status(201).json("Cart created");
+      } else {
+        // Update existing cart
+        const existingProduct = userCart.products.find(
+          (product) => product.product_id === product_id
+        );
+  
+        if (existingProduct) {
+          existingProduct.quantity += quantity; // Update quantity
+        } else {
+          userCart.products.push({
+            product_id,
+            quantity,
+          });
         }
-        user.save()
-        res.json("cart updated")
+  
+        await userCart.save();
+        return res.status(200).json("Cart updated");
+      }
+    } catch (err) {
+      console.error("Error updating cart:", err);
+      return res.status(500).json("Server Error");
     }
-    
+  };
 
-}
 
 exports.getcart=async(req,res)=>{
     const userid=req.user.userid;
